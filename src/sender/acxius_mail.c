ab1dde0197981a6fe8b25b2c0ce63b5510821747
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-12-12 14:17:57 -0300

    chore: move email credentials to environment variables

--- src/sender/acxius_mail.c
+++ src/sender/acxius_mail.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <curl/curl.h>
 #include "acxius_mail.h"
 
@@ -11,27 +12,28 @@ void sendEmail(const char *to, const char *subject, SchedulerData *data)
     char message[1024];
     
     snprintf(message, sizeof(message),
-             "From: email@\n"
+             "From: %s\n"
              "To: <%s>\n"
              "Subject: %s\n\n"
              "Generate: ['%s', '%s', '%s']\n",
-             to, subject, data->msg, data->date, data->send);
+             getenv("ACX_EMAIL"), to, subject, data->msg, data->date, data->send);
     
     curl = curl_easy_init();
     if(curl) {
+        const char *email = getenv("ACX_EMAIL");
+        const char *password = getenv("ACX_PASSWORD");
+
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
         curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        curl_easy_setopt(curl, CURLOPT_USERNAME, "email@");
-        curl_easy_setopt(curl, CURLOPT_PASSWORD, "");
-
+        curl_easy_setopt(curl, CURLOPT_USERNAME, email);
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, password);
         curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:465");
-        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, "meiryannempereira@gmail.com");
+        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, email);
         
         recipients = curl_slist_append(recipients, to);
         curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
 
         curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
-        
         curl_easy_setopt(curl, CURLOPT_READDATA, message);
         curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
 
